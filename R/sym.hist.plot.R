ce2cadc72a34c6a378835b88439060870f6c7921
Andres-16 <andres.navarro.duran16@gmail.com>
2018-06-28 09:08:00 -0600

    Se aplica redondeo al gráfico de histogramas (plot)

--- R/sym.hist.plot.R
+++ R/sym.hist.plot.R
@@ -5,30 +5,19 @@ sym.hist.plot <- function(info, col=c("blue"), border=FALSE, show.type = TRUE){
     stop("The data type is wrong, only $H are accepted")
 
   dataset <- as.matrix(info$data) #obtenemos los datos
-  # namesC <- colnames(dataset) #obtenemos los nombres
-  # matches <- regmatches(namesC, gregexpr("[[:digit:]]+", namesC)) #obtenemos los numeros de los nombres
-  # dataTemp <- c() #creamos array temp
+  namesC <- colnames(dataset) #obtenemos los nombres
+  matches <- regmatches(namesC, gregexpr("[[:digit:]]+\\.?[[:digit:]]+", namesC)) #obtenemos los numeros de los nombres
+  breaks <- as.numeric(unlist(matches))
+  i <- seq(1,length(breaks),2)
+  j <- seq(2,length(breaks),2)
+  bins <- gsub("\\s","",paste(format(breaks[i], scientific=F, format="f", digits=2),format(breaks[j], scientific=F,format="f", digits=2),sep = ","))
+  names <- c(paste0("[",bins[-length(bins)],")"),paste0("[",bins[length(bins)],"]"))
 
-  # for (index in 1:length(matches)) { #genera un array representativo
-  #   interval <- as.numeric(matches[[index]])
-  #   if(dataset[1,index]!=0)
-  #     dataTemp <- c(dataTemp, runif(100*dataset[1,index], interval[1], interval[2]))
-  # }
-
-  # breaks <- unique(as.numeric(unlist(matches))) # los breaks son los nombres sin repeticiones
-
-  #grafica el histograma
-  # hist(dataTemp, breaks = breaks,
-  #      freq = T, xlim = c(min(breaks), max(breaks))+c(-1,1),ylim = c(0,100),
-  #      border = "black",col = col, yaxt="n",xlab = "", ylab="",
-  #      main = paste(info$sym.var.names,ifelse(show.type," (Histogram)","")))
-  # graphics::axis(2, at=seq(0, 100, 20), labels=sprintf(round(seq(0, 100, 20)), fmt="%2.f%%"))#los y labels con %
-
-  plt <- barplot(dataset,ylim = c(0,1), names.arg = colnames(dataset), col = col,
+  plt <- barplot(dataset,ylim = c(0,1), names.arg = names, col = col,
                  yaxt="n",xaxt="n",xlab = "", ylab="",
                  main = paste(info$sym.var.names,ifelse(show.type," (Histogram)","")))
   graphics::axis(2, at=seq(0, 1, 0.2), labels=sprintf(round(seq(0, 100, 20)), fmt="%2.f%%"), las=1)
-  text(plt, par("usr")[3], labels = colnames(dataset), srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=0.7)
+  text(plt, par("usr")[3], labels = names, srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=0.7)
 
   if(border) #se pone el borde en negro
     box("figure", col="black")
