86e728a82a0d37869841c4e6004f25b23d4b57ab
Carlos Agüero Barquero <dev.aguero@gmail.com>
2019-03-08 13:56:04 -0600

    replica del archivo utilizado por billard para demostrar distancias entre intervalos

---
+++ test_data/animals_billard.R
@@ -0,0 +1,10 @@
+library(tibble)
+library(ggplot2)
+
+animals_df <- tibble(Animal = c("Horse M","Horse M",
+                                "Horse F","Horse F",
+                                "Bear M","Bear M"),
+       Heigth = c(120,180,158,160,175,185),
+       Weight = c(222.2,354,322,355,117.2,152))
+
+
